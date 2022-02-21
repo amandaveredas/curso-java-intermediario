a783122014ac4527f3115d8f4489e80dc6d1c096
amandaveredas <amandaveredas@gmail.com>
2022-02-21 15:02:02 -0300

    FEAT: métodos abstratas e enum
    Implementa o método abstrato gerarNumero() no enum TipoDocumento

---
+++ src/com/amanda/javaintermediario/Pessoa.java
@@ -0,0 +1,39 @@
+package com.amanda.javaintermediario;
+
+public class Pessoa {
+
+    private TipoDocumento tipoDocumento;
+    private String numeroDoDocumento;
+
+    public Pessoa(TipoDocumento tipoDocumento, String numeroDoDocumento) {
+        this.tipoDocumento = tipoDocumento;
+        this.numeroDoDocumento = numeroDoDocumento;
+    }
+
+    public Pessoa() {
+    }
+
+    public TipoDocumento getTipoDocumento() {
+        return tipoDocumento;
+    }
+
+    public void setTipoDocumento(TipoDocumento tipoDocumento) {
+        this.tipoDocumento = tipoDocumento;
+    }
+
+    public String getNumeroDoDocumento() {
+        return numeroDoDocumento;
+    }
+
+    public void setNumeroDoDocumento(String numeroDoDocumento) {
+        this.numeroDoDocumento = numeroDoDocumento;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "tipoDocumento=" + tipoDocumento +
+                ", numero='" + numeroDoDocumento + '\'' +
+                '}';
+    }
+}
---
+++ src/com/amanda/javaintermediario/TesteDocumento.java
@@ -0,0 +1,20 @@
+package com.amanda.javaintermediario;
+
+public class TesteDocumento {
+
+    public static void main(String[] args) {
+        for(TipoDocumento doc : TipoDocumento.values()){
+            System.out.println(doc + " - " + doc.geraNumeroTeste());
+        }
+
+        Pessoa pessoaFisica = new Pessoa();
+        pessoaFisica.setTipoDocumento(TipoDocumento.CPF);
+        pessoaFisica.setNumeroDoDocumento(pessoaFisica.getTipoDocumento().geraNumeroTeste());
+        System.out.println(pessoaFisica);
+
+        Pessoa pessoaJuridica = new Pessoa();
+        pessoaJuridica.setTipoDocumento(TipoDocumento.CNPJ);
+        pessoaJuridica.setNumeroDoDocumento(pessoaJuridica.getTipoDocumento().geraNumeroTeste());
+        System.out.println(pessoaJuridica);
+    }
+}
